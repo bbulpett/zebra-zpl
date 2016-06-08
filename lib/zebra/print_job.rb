e7f3204e4d9625d3b5e8fcf070361101bf2ac6d6
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-06-08 17:25:40 -0400

    Change hardcoded ip

--- lib/zebra/print_job.rb
+++ lib/zebra/print_job.rb
@@ -28,7 +28,7 @@ module Zebra
 
     def send_to_printer(path)
       # My ip is 192.168.101.99
-      `lp -h 192.168.101.99 -d #{@printer} -o raw #{path}`
+      `lp -h 192.168.101.128 -d #{@printer} -o raw #{path}`
       # if RUBY_PLATFORM =~ /darwin/
       #   `lpr -h 192.168.101.99 -P #{@printer} -o raw #{path}`
       # else
