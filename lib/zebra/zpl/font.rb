d4f410acdc2b5a57874d5e6dc3b44f1777faafeb
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-06-28 17:52:09 -0400

    Add larger font sizes

--- lib/zebra/zpl/font.rb
+++ lib/zebra/zpl/font.rb
@@ -10,9 +10,11 @@ module Zebra
       SIZE_5 = 44 # 16pt
       SIZE_6 = 67 # 24pt
       SIZE_7 = 100 # 32pt
+      SIZE_8 = 111 # 40pt
+      SIZE_9 = 133 # 48pt
 
       def self.valid_font_size?(font_size)
-        [17, 22, 28, 33, 44, 67, 100].include?(font_size.to_i) || ('A'..'Z').include?(font_size)
+        [17, 22, 28, 33, 44, 67, 100, 111, 133].include?(font_size.to_i) || ('A'..'Z').include?(font_size)
       end
 
       def self.validate_font_size(font_size)
