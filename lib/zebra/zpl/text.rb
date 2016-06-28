05602e8318cbf1a784be863140cd5f97417c0b3c
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-06-28 15:31:13 -0400

    Modify text_to_zpl return string

--- lib/zebra/zpl/text.rb
+++ lib/zebra/zpl/text.rb
@@ -46,7 +46,7 @@ module Zebra
       def to_zpl
         check_attributes
         # ["A#{x}", y, rotation, font_size, h_multiplier, v_multiplier, print_mode, "\"#{data}\""].join(",")
-        "^FB800,100,0,C,0^FD#{data}^FS"
+        "^FO#{x},#{y}^FB600,100,0,C,0^FD#{data}^FS"
       end
 
       private
