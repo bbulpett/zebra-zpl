a86e16968d19c79966838c51be6845345a9105f6
Barnabas Bulpett <bbulpett@charter.net>
2016-06-29 11:12:15 -0400

    Remove duplicate font size attribute from text block builder

--- lib/zebra/zpl/text.rb
+++ lib/zebra/zpl/text.rb
@@ -57,7 +57,7 @@ module Zebra
         # ["A#{x}", y, rotation, font_size, h_multiplier, v_multiplier, print_mode, "\"#{data}\""].join(",")
         # "^FO25,25^FB600,100,0,C,0^FDFoo^FS"
 
-        "^CF#{font_type},#{font_size},#{font_size}^FO#{x},#{y}^FB800,4,0,#{justification},0^FD#{data}^FS"
+        "^CF#{font_type},#{font_size}^FO#{x},#{y}^FB800,4,0,#{justification},0^FD#{data}^FS"
       end
 
       private
