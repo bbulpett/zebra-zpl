08d9f4a441ae90d54d9c42c6fbffe49d30f7debb
Barnabas Bulpett <bbulpett@charter.net>
2017-10-27 16:41:04 -0400

    Add width assignment method to Raw module

--- lib/zebra/zpl/raw.rb
+++ lib/zebra/zpl/raw.rb
@@ -5,16 +5,21 @@ module Zebra
     class Raw
       include Printable
 
+      attr_reader :width
+
+      def width=(width)
+        unless margin.nil? || margin < 1
+          @width = (width - (margin * 2))
+        else
+          @width = width || 0
+        end
+      end
+
       def to_zpl
         # check_attributes
         "^FW#{rotation}^FO#{x},#{y}#{data}^FS"
       end
 
-      # private
-
-      # def check_attributes
-      #   super
-      # end
     end
   end
 end
