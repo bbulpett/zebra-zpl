1806389262db40e7460822ecfc6a041748d4380d
Barnabas Bulpett <bbulpett@charter.net>
2017-10-27 15:07:40 -0400

    Add raw.rb file

---
+++ lib/zebra/zpl/raw.rb
@@ -0,0 +1,20 @@
+require "zebra/zpl/printable"
+
+module Zebra
+  module Zpl
+    class Raw
+      include Printable
+
+      def to_zpl
+        check_attributes
+        "^FW#{rotation}^FO#{x},#{y}#{data}^FS"
+      end
+
+      private
+
+      def check_attributes
+        super
+      end
+    end
+  end
+end
