fb458ec8f048aac8c718ee9d7755288e12929f11
Barnabas Bulpett <bbulpett@charter.net>
2017-10-27 15:16:53 -0400

    Define width on Raw module

--- lib/zebra/zpl/raw.rb
+++ lib/zebra/zpl/raw.rb
@@ -5,6 +5,16 @@ module Zebra
     class Raw
       include Printable
 
+      attr_reader :width
+
+      def width=(width)
+        unless (margin.nil? || margin < 1)
+          @width = (width - (margin*2))
+        else
+          @width = width || 0
+        end
+      end
+
       def to_zpl
         check_attributes
         "^FW#{rotation}^FO#{x},#{y}#{data}^FS"
