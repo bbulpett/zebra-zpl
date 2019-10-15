24bdacdd97612e0497474b33b8f43ca099393415
LagTag <loganagreen95@gmail.com>
2019-10-14 22:25:29 -0400

    Consolidate graphic elements into one class

---
+++ lib/zebra/zpl/graphic.rb
@@ -0,0 +1,83 @@
+require "zebra/zpl/printable"
+
+module Zebra
+  module Zpl
+    class Graphic
+      include Printable
+
+      class InvalidLineThickness < StandardError; end
+      class InvalidColorError < StandardError; end
+      class InvalidOrientationError < StandardError; end
+      class InvalidGraphicType < StandardError; end
+
+      alias_attribute :diameter, :graphic_width
+
+      attr_reader :line_thickness, :graphic_width, :graphic_height, :color, :orientation, :rounding_degree, :graphic_type
+
+      ELIPSE   = "E"
+      BOX      = "B"
+      DIAGONAL = "D"
+      CIRCLE   = "C"
+      SYMBOL   = "S"
+
+      def graphic_type=(type)
+        raise InvalidGraphicType unless %w(E B D C S).include? type
+        @graphic_type = type
+      end
+
+      def self.valid_graphic_type?(type)
+        
+      end
+
+      def line_thickness=(thickness)
+        raise InvalidLineThickness unless thickness.nil? || thickness.to_i.to_s == thickness.to_s
+        @line_thickness = thickness
+      end
+
+      def graphic_width=(width)
+        @graphic_width = width
+      end
+
+      def graphic_height=(height)
+        @graphic_height = height
+      end
+
+      def color=(value)
+        raise InvalidColorError unless value&.upcase.in?(["B","W"])
+        @color = value
+      end
+
+      def orientation=(value)
+        raise InvalidOrientationError unless value&.upcase.in?(["R","L"])
+        @orientation = value
+      end
+
+      def to_zpl
+        check_attributes
+        zpl = case graphic_type 
+        when "B"
+            "D#{graphic_width},#{graphic_height},#{line_thickness},#{color},#{orientation}"
+        when "E"
+            "E#{graphic_width},#{graphic_height},#{line_thickness},#{color}"
+        when "C"
+            "C#{diameter},#{line_thickness},#{color}"
+        when "D"
+            "D#{graphic_width},#{graphic_height},#{line_thickness},#{color},#{orientation}"
+        when "S"
+            "S#{orientation},#{graphic_height},#{graphic_width}"
+        end
+        "^FO#{x},#{y}^G#{zpl}^FS"
+      end
+
+      private
+
+      def has_data?
+        false
+      end
+
+      def check_attributes
+        super
+      end
+    end
+  end
+end
